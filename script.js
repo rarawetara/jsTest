55f4941c126b78d2007d32cf464f0239e2af86a8
rarawetara <rarawetara@gmail.com>
2023-05-05 00:22:03 +0300

    new calc

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 
-  
-function Add(a, b){
-    return a + b;
-  }
-  
-  function Sub(a, b){
-    return a - b;
-  }
-  
-  function Mul(a, b){
-    return a * b;
-  }
-  
-  function Div(a, b){
-    if(b == 0){
-     return 'Error';
-    } else{
-       return a / b;
-    }
-  }
-  
-  var a = (prompt('firstNumber'));
-  var b = (prompt('SecondNumber'));
-  document.write('Sub: ' + Sub(a, b) + '<br>');
-  document.write('Add: ' + Add(a, b) + '<br>');
-  document.write('Mul: ' + Mul(a, b) + '<br>');
-  document.write('Div: ' + Div(a, b) + '<br>');
+const operator = prompt('Enter operator ( operator +, -, * or / ): ');
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
 
+switch(operator) {
+  case '+':
+  result = number1 + number2;
+  console.log(`${number1} + ${number2} = ${result}`);
+  break;''
+
+  case '-':
+  result = number1 - number2;
+  console.log(`${number1} - ${number2} = ${result}`);
+  break;
+
+  case '*':
+  result = number1 * number2;
+  console.log(`${number1} * ${number2} = ${result}`);
+  break;
+
+  case '/':
+  result = number1 / number2;
+  console.log(`${number1} / ${number2} = ${result}`);
+  break;
+
+  default:
+  console.log('error');
+  break;
+   }
   /////////////////////////////////////////////////
-  let a = +prompt("перше число");
-  let b = +prompt("друге число");
-  let с = +prompt("трете число");
+  let a = +prompt("першеЧисло");
+  let b = +prompt("другеЧисло");
+  let с = +prompt("третеЧисло");
   alert( (a + b +с) /3);
   
   
