4884f76c50bd5bf939f0a5dde6c8007c1c8cab74
rarawetara <rarawetara@gmail.com>
2023-05-07 12:16:46 +0300

    done

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
+const city = prompt("enter  your city?" );
+if (!city) alert('Its a shame that you didnt specify  city');
+ let favoritesport = prompt('enter your favorite sport');
+if (!favoritesport) alert('Its a shame that you didnt specify favorite sport');
 
-  
-function Add(a, b){
-    return a + b;
-  }
-  
-  function Sub(a, b){
-    return a - b;
-  }
-  
-  function Mul(a, b){
-    return a * b;
-  }
-  
-  function Div(a, b){
-    if(b == 0){
-     return 'Error';
-    } else{
-       return a / b;
-    }
-  }
-  
-  var a = (prompt('firstNumber'));
-  var b = (prompt('SecondNumber'));
-  document.write('Sub: ' + Sub(a, b) + '<br>');
-  document.write('Add: ' + Add(a, b) + '<br>');
-  document.write('Mul: ' + Mul(a, b) + '<br>');
-  document.write('Div: ' + Div(a, b) + '<br>');
+  const  year = +prompt('enter your year of birth');
+if (!year) alert('Its a shame that you didnt specify year of birth');
+   let newYear = year ? `${'You age'} ${2023 - year}`:'' ;
 
-  /////////////////////////////////////////////////
-  let a = +prompt("перше число");
-  let b = +prompt("друге число");
-  let с = +prompt("трете число");
-  alert( (a + b +с) /3);
+ 
+ const newCity = city 
+switch (newCity){
+ case '':
+    alert(`${newYear}`);
+   break;
+
+  case 'kiev':
+    alert(`${newYear} ${'lucky you live in Ukraine'}`);
+ 
+   break;
+  case 'vien':
+    alert( `${newYear} ${'lucky you live in Austria'}`);
+    break;
+
+  case 'warshawa':
+    alert( `${newYear} ${'lucky you live in Poland'}` );
+    break;
   
-  
+}
+
+
+
+ switch (favoritesport) {
+  case 'football':
+    alert( 'You gonna be a great football player' );
+    break;
+
+  case 'basketball':
+    alert( 'You gonna be a great basketball player' );
+    break;
+
+  case 'box':
+    alert( 'You gonna be a great boxer' );
+    break;
+ 
+}
+
